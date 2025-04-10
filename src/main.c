711b638b31e034770b8dd13c93eb99896403f9b5
vaquitson <vaquitson@gmail.com>
2025-04-09 21:16:01 -0400

    basic test version

--- src/main.c
+++ src/main.c
@@ -1,114 +1,97 @@
 #include <stdio.h>
-#include <sys/inotify.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <poll.h>
 #include <unistd.h>
-#include <errno.h>
-#include "cmon_errors.h"
+#include <wait.h>
 
-bool keepRunning = 1;
+#include <sys/inotify.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <poll.h>
 
-void termination_cb(int sigNum){
-  printf("ended\n");
-  keepRunning = 0;
-}
-
-void handle_events(int inFd, int *wd, int argc, char **arg){
-  printf("event handeling");
-  char buff[4096];
-  // __attribute__ ((aligned(__alignof__(struct inotify_event))));
-
-  const struct inotify_event *event;
-  ssize_t size;
-  ssize_t u = 1;
-
-  while (1){
-    size = read(inFd, buff, sizeof(buff));
-    // erno again is set wen the read dosent find enithing to read
-    // but is not an error
-    if (size == -1 && errno != EAGAIN){
-      cmon_print_error(true, "handle_events", "canot read the event");
-      return;
-    }
-
-    if (size <= 0){
-      cmon_print_error(true, "handle_events", "the read function return an error");
-      printf("size %zu\n", u);
-      return;
-    }
-    
-    // loop through the events in the buffer
-    for (char *ptr = buff; ptr < buff; ptr += sizeof(struct inotify_event) + event->len){
-      event = (const struct inotify_event *) ptr; 
- 
-      if (event->mask & IN_OPEN){
-        printf("IN_OPEN: ");
-
-      } else if (event->mask & IN_CLOSE_NOWRITE){
-        printf("IN_CLOSE_NOWRITE: ");
-
-      } else if (event->mask & IN_CLOSE_WRITE){
-        printf("IN_CLOSE_WRITE: ");
-      } else if (event->mask & IN_MODIFY){
-        printf("IN_MODIFY: ");
-      }
-    }
-  }
-}
-
-
-int main(int argc, char **argv){
-  char testPath[] = "/home/narval/programing/proyects/cmon/test_file.txt";
-  unsigned short int inotifyFlags = IN_NONBLOCK;
-  struct sigaction terminationAction = {0};
-  int inFd;
-  int pollNum;
-  int *watchDirectories;
-  nfds_t nfds = 2;
-  struct pollfd fds[nfds];
-  const uint32_t eventWatchMask = IN_MODIFY | IN_OPEN | IN_ACCESS;
-
-  terminationAction.sa_handler = termination_cb; 
-  terminationAction.sa_flags = 0;
-
-  if (sigaction(SIGINT, &terminationAction, NULL) != 0){
-    cmon_print_error(true, "main", "could not assign the termination cb to SIGINT"); 
+// open a inotify instance
+int open_inotify_fd(){
+  int fd;
+  fd = inotify_init();
+  if (fd < 0){
+    printf("inotify could not be instanciated\n");
     return -1;
-  } 
-  
-  watchDirectories = calloc(1, sizeof(int)); 
-  if (!watchDirectories){
-    cmon_print_error(true, "main", "could not allocate size for the watchDirectories array");
-    return -2;
   }
+  return fd;
+}
 
-  inFd = inotify_init1(inotifyFlags);
 
-  fds[0].fd = STDIN_FILENO;
+// watch the test directory
+int watch_dir(int inFd, char *path, unsigned long mask){
+  int wd; // watch descriptor
+  wd = inotify_add_watch(inFd, path, mask);
+  if (wd < 0){
+    printf("The path could not be watched\n");
+    return -1;
+  }
+  return wd;
+}
+
+
+// starts the child process, wiich is a node server
+int start_child_process(){
+  pid_t pid;
+  pid = fork();
+  if (pid == 0){
+    // the second argument is the name of the process, and the next is the parameter
+    execlp("node", "juanpablosegundo", "/home/narval/programing/learning/inotify/inotify_creating_new_precees/test.js", NULL);
+    return 0;
+  } else if (pid < 0){
+    return -1;
+  } else {
+    return pid;
+  }
+}
+
+
+void procees_events(size_t readSize, char *buff, pid_t *subPPid){
+  static const struct inotify_event *event;
+
+  for (char *ptr = buff; ptr < buff + readSize; ptr += sizeof(struct inotify_event) + event->len){
+    event = (const struct inotify_event *) ptr;
+    printf("event: %s\n", event->name);
+    if (event->mask & IN_MODIFY) {
+      kill(*subPPid, SIGKILL);
+      *subPPid = start_child_process();  
+    }
+  }
+}
+
+
+int main(){
+  int inotifyFd;
+  int watchFileList[2] = {0};
+  unsigned long mask =  IN_MODIFY;
+  const struct inotify_event *event;
+  char buff[4096];
+  size_t readSize;
+  int ret;
+  struct pollfd fds[1];
+  pid_t subPPid;
+
+  inotifyFd = open_inotify_fd();
+
+  fds[0].fd = inotifyFd;
   fds[0].events = POLLIN;
 
-  fds[1].fd = inFd;
-  fds[1].events = POLLIN;
-  
-  // traking files
-  watchDirectories[0] = inotify_add_watch(inFd, testPath, eventWatchMask);
-  if (watchDirectories[0] == -1){
-    cmon_print_error(true, "main", "coudl not watch the dir");
-  }
+  watchFileList[0] = watch_dir(inotifyFd, "/home/narval/programing/proyects/cmon/build/test.js", mask);
 
-  printf("Listening for events.\n");
-  while (keepRunning){ 
-    pollNum = poll(fds, nfds, -1);
-    printf("hello");
-    if (pollNum > 0){
-      if (fds[1].revents & POLLIN){
-        printf("FILEE\n");
-        handle_events(inFd, watchDirectories, -1, NULL);
+  subPPid = start_child_process(); 
+
+  while (1){
+    ret = poll(fds, 1, -1);
+
+    if (ret > 0 && fds[0].revents & POLLIN){
+      readSize = read(inotifyFd, buff, sizeof(buff));
+      if (readSize <= 0){
+        printf("Reading error\n");
       }
+      procees_events(readSize, buff, &subPPid);
+    } else {
+      continue;
     }
   }
-
-  return 0;
 }
